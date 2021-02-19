45d8de0c58d3963fc0404c40a08b774f843174a6
StellaGreen <vickysoler5@gmail.com>
2021-02-19 15:09:48 +0100

    omg

--- crackme04.js
+++ crackme04.js
@@ -1,6 +1,6 @@
 // Vous devez assigner la bonne string à la variable password ci dessous
 // Et obtenir le message 'OK' en éxecutant le programme
-let password = 'asdasdadfderc'
+let password = ''
 
 const crackme4 = (password) => {
   let tmp1 = 0

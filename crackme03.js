e65e3a91de383309d935bccfa542cfcbef48e431
StellaGreen <vickysoler5@gmail.com>
2021-02-19 14:07:29 +0100

    add answer

--- crackme03.js
+++ crackme03.js
@@ -1,6 +1,6 @@
 // Vous devez assigner la bonne string à la variable password ci dessous
 // Et obtenir le message 'OK' en éxecutant le programme
-let password = ''
+let password = '@99!asdfbvea ?'
 
 const crackme3 = (password) => {
   if (password.length === 14 && password[0] === '@' && password[3] === '!') {

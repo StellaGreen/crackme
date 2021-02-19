48861cbcb5cb53c202aee77b8eed27373e7f6779
StellaGreen <vickysoler5@gmail.com>
2021-02-19 13:59:41 +0100

    add answer

--- crackme05.js
+++ crackme05.js
@@ -1,6 +1,6 @@
 // Vous devez assigner la bonne string à la variable password ci dessous
 // Et obtenir le message 'OK' en éxecutant le programme
-let password = ''
+let password = '4321drowssap'
 
 const crackme5 = (password) => {
   if (password.split('').reverse().join('') === 'password1234') {

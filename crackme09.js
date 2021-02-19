c3f9290be312e5d5c5a0efc166c3321918f2e316
StellaGreen <vickysoler5@gmail.com>
2021-02-19 15:45:01 +0100

    add answer

--- crackme09.js
+++ crackme09.js
@@ -1,6 +1,6 @@
 // Vous devez assigner la bonne string à la variable password ci dessous
 // Et obtenir le message 'OK' en éxecutant le programme
-let password = ''
+let password = 'JustASimplePassword'
 
 const crackme9 = (password) => {
   if (password.startsWith('Just') && password.endsWith('Password')) {
